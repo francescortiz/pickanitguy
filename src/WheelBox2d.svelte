1f5353f75ed11cefdbee62bc909adc95d1ba3a72
Francesc Ortiz <francesc@VideoPal.local>
2024-12-25 08:46:07 +0100

    feat: Try to make the wheel, but I couldn't find how to make the joins fixed, they are always elastic.

--- src/WheelBox2d.svelte
+++ src/WheelBox2d.svelte
@@ -13,27 +13,50 @@
 	import Box2DFactory from 'box2d-wasm';
 	import { makeDebugDraw } from './debugDraw';
 
-	interface WheelBox2dProps {
+	type WheelBox2dProps = {
 		session: AuthSession;
 		appSupabase: SupabaseClient<Database>;
 	}
 
+	enum CollisionCategory {
+		NoCollisions = 0b0000,
+		Wheel = 0b0000,
+		Pegs = 0b0010,
+	}
+
+	enum CollisionMasks {
+		NoCollisions = 0b0000,
+		Wheel = 0b0000,
+		Pegs = 0b0001
+	}
+
 	const { appSupabase, session }: WheelBox2dProps = $props();
 
-	let canvas: HTMLCanvasElement | null = $state(null);
+	let canvasRef: HTMLCanvasElement | null = $state(null);
 
-	$effect (() => {
+	$effect(() => {
 
-		if (canvas === null) {
-			return
+		if (canvasRef === null) {
+			return;
 		}
 
-		const ensuredCanvas: HTMLCanvasElement = canvas
+		const canvas: HTMLCanvasElement = canvasRef;
 
 		Box2DFactory().then((box2D: typeof Box2D & EmscriptenModule) => {
-			const { b2BodyDef, b2_dynamicBody, b2_staticBody, b2PolygonShape, b2Vec2, b2World } = box2D;
+			const {
+				b2BodyDef,
+				b2Filter,
+				b2_dynamicBody,
+				b2_staticBody,
+				b2PolygonShape,
+				b2CircleShape,
+				b2Vec2,
+				b2World,
+				b2RevoluteJointDef,
+				b2WeldJointDef,
+			} = box2D;
 
-			const ctx = ensuredCanvas.getContext('2d');
+			const ctx = canvas.getContext('2d');
 
 			if (!ctx) {
 				throw new Error('Unable to get 2d context from canvas');
@@ -52,19 +75,150 @@
 			const debugDraw = makeDebugDraw(ctx, pixelsPerMeter, box2D);
 			world.SetDebugDraw(debugDraw);
 
+			/**
+			 * Weehl
+			 */
+
+
+
+
+			const wheelBase = (() => { // Wheel base
+				const wheelBasePosition = new b2Vec2(0, 0);
+
+				const wheelBaseDefinition = new b2BodyDef();
+				wheelBaseDefinition.set_type(b2_staticBody);
+				wheelBaseDefinition.set_position(wheelBasePosition);
+
+				const wheelBaseShape = new b2PolygonShape();
+				wheelBaseShape.SetAsBox(0.25, 0.25);
+
+				const wheelBase = world.CreateBody(wheelBaseDefinition);
+
+				const wheelBaseFixture = wheelBase.CreateFixture(wheelBaseShape, 1);
+				const wheelBaseFilter = new b2Filter();
+				wheelBaseFilter.set_categoryBits(CollisionCategory.NoCollisions);
+				wheelBaseFilter.set_maskBits(CollisionMasks.NoCollisions);
+				wheelBaseFixture.SetFilterData(wheelBaseFilter);
+
+				// wheelBase.SetTransform(wheelBasePosition, 0);
+				// wheelBase.SetLinearVelocity(boxPosition);
+				wheelBase.SetAwake(true);
+				wheelBase.SetEnabled(true);
+
+				return wheelBase;
+			})();
+
+
+			const wheelRadius = 2.50;
+
+			const wheel = (() => { // Wheel
+				const wheelPosition = new b2Vec2(0, wheelRadius);
+
+				const wheelDefinition = new b2BodyDef();
+				wheelDefinition.set_type(b2_dynamicBody);
+				wheelDefinition.set_position(wheelPosition);
+
+				const wheelShape = new b2CircleShape();
+				wheelShape.set_m_radius(wheelRadius);
+
+				const wheel = world.CreateBody(wheelDefinition);
+
+				const wheelFixture = wheel.CreateFixture(wheelShape, 1);
+				const wheelFilter = new b2Filter();
+				wheelFilter.set_categoryBits(CollisionCategory.Wheel);
+				wheelFilter.set_maskBits(CollisionMasks.Wheel);
+				wheelFixture.SetFilterData(wheelFilter);
+				wheelFixture.SetDensity(0.5)
+				wheelFixture.SetFriction(0.5);
+
+				// wheel.SetTransform(wheelBasePosition, 0);
+				// wheel.SetLinearVelocity(boxPosition);
+				wheel.SetAwake(true);
+				wheel.SetEnabled(true);
+
+				return wheel;
+			})();
+
+			const pegFilter = new b2Filter();
+			pegFilter.set_categoryBits(CollisionCategory.Pegs);
+			pegFilter.set_maskBits(CollisionMasks.Pegs);
+
+			{
+				const pegCount = 12;
+				const pegAngleDelta = Math.PI * 2 / pegCount;
+				for (let i = 0; i < pegCount; i++) {
+					const pegRadius = 0.05;
+					const pegOffset = (wheelRadius - pegRadius * 2);
+					const angle = pegAngleDelta * i;
+					const pegX = Math.cos(angle) * pegOffset;
+					const pegY = Math.sin(angle) * pegOffset;
+					const pegPosition = new b2Vec2(pegX, pegY);
+
+					const pegDefinition = new b2BodyDef();
+					pegDefinition.set_type(b2_dynamicBody);
+					pegDefinition.set_position(pegPosition);
+					pegDefinition.set_bullet(true);
+
+					const pegShape = new b2CircleShape();
+					pegShape.set_m_radius(pegRadius);
+
+					const peg = world.CreateBody(pegDefinition);
+
+					const pegFixture = peg.CreateFixture(pegShape, 1);
+					pegFixture.SetFilterData(pegFilter);
+					pegFixture.SetDensity(0.5);
+					pegFixture.SetFriction(0.5);
+
+
+					const pegJointDefinition = new b2RevoluteJointDef();
+					// pegJointDefinition.set_collideConnected(true)
+					pegJointDefinition.set_bodyA(peg);
+					pegJointDefinition.set_bodyB(wheel);
+					pegJointDefinition.set_localAnchorA(new b2Vec2(0,0));
+					pegJointDefinition.set_localAnchorB(pegPosition);
+					// pegJointDefinition.set_stiffness(0);
+					// pegJointDefinition.set_damping(1.0);
+					pegJointDefinition.set_enableLimit(true)
+					pegJointDefinition.set_upperAngle(0)
+					pegJointDefinition.set_lowerAngle(0)
+					pegJointDefinition.set_referenceAngle(wheel.GetAngle() - peg.GetAngle())
+					pegJointDefinition.set_collideConnected(false)
+					const pegJoint = world.CreateJoint(pegJointDefinition);
+
+				}
+			}
+
+
+			// { // Wheel joint
+			// 	const wheelJointDefinition = new b2RevoluteJointDef();
+			// 	wheelJointDefinition.set_bodyA(wheel);
+			// 	wheelJointDefinition.set_bodyB(wheelBase);
+			// 	const wheelJoint = world.CreateJoint(wheelJointDefinition);
+			// }
+
+
+			wheel.ApplyAngularImpulse(120, true);
+
+
+			/**
+			 * EXAMPLE
+			 */
+
+
 			const sideLengthMetres = 1;
 			const boxShape = new b2PolygonShape();
 			boxShape.SetAsBox(sideLengthMetres / 4, sideLengthMetres / 4);
 
 			const zero = new b2Vec2(0, 0);
-			const boxPosition = new b2Vec2(0, -3);
+			const boxPosition = new b2Vec2(3, -3);
 
 			const boxDefinition = new b2BodyDef();
 			boxDefinition.set_type(b2_dynamicBody);
 			boxDefinition.set_position(boxPosition);
 
 			const box = world.CreateBody(boxDefinition);
-			box.CreateFixture(boxShape, 1);
+			const boxFixture = box.CreateFixture(boxShape, 1);
+			boxFixture.SetDensity(100)
 			box.SetTransform(boxPosition, 0);
 			box.SetLinearVelocity(boxPosition);
 			box.SetAwake(true);
@@ -131,7 +285,7 @@
 
 			const drawCanvas = () => {
 				ctx.fillStyle = 'rgb(0,0,0)';
-				ctx.fillRect(0, 0, ensuredCanvas.width, ensuredCanvas.height);
+				ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 				ctx.save();
 				ctx.scale(pixelsPerMeter, pixelsPerMeter);
@@ -175,7 +329,7 @@
 				box.SetLinearVelocity(boxLinerVelocity);
 			};
 
-			ensuredCanvas.onclick = () => {
+			canvas.onclick = () => {
 				jump();
 			};
 
@@ -204,4 +358,4 @@
 
 </script>
 
-<canvas bind:this={canvas} width="800" height="700"></canvas>
+<canvas bind:this={canvasRef} width="800" height="700"></canvas>
